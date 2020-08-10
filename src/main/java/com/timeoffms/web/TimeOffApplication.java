988ae106ce1a6d52315064ba6062175326751ba9
Jose Mora <jdevmora@gmail.com>
2020-08-10 03:42:48 -0600

    Adding first application files

---
+++ src/main/java/com/timeoffms/web/TimeOffApplication.java
@@ -0,0 +1,13 @@
+package com.timeoffms.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeOffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeOffApplication.class, args);
+	}
+
+}
