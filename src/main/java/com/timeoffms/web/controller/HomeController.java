dc21823ec64ab63c630f72f23e95b857bc17be0e
Jose Mora <jdevmora@gmail.com>
2020-08-19 04:05:01 -0600

    Created login, templates and default styles.

---
+++ src/main/java/com/timeoffms/web/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.timeoffms.web.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@Log4j2
+public class HomeController {
+
+   @GetMapping("/login")
+   public ModelAndView showLogin(ModelMap model) {
+       return new ModelAndView("login/login", model);
+   }
+}
